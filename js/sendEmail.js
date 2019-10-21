4ba3ce50a4bc07429a6ef2cf84755ef38ff07e97
Max Miguta <max_miguta@yahoo.com>
2019-10-21 18:53:59 +0100

    Fixed EmailJS error

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,19 +1,18 @@
-function sendMail(contactForm) {
-    var service_id = "default_service";
-	var template_id = "contact_me";
-    emailjs.sendForm("default_service", "contact_me", "#enquiryForm", {
-        "first_name": contactForm.first_name.value,
-        "last_name": contactForm.last_name.value,
-        "from_email": contactForm.email_address.value,
-        "enquiry": contactForm.enquiry.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
+emailjs.init("user_qJBP2xc5mt95iQW1Rq1zm");
+
+const form = document.querySelector('form');
+form.onsubmit = (event) => sendMail(event);
+
+const sendMail = async (event) => {
+    event.preventDefault();
+    const mail = await emailjs.sendForm(
+        'default_service',
+        'contact_me',
+        event.target,
+        "user_qJBP2xc5mt95iQW1Rq1zm"
     );
-    return false;  // To block from loading a new page
-}
+	console.log(mail)
+	event.target.reset()
+	if (mail.status = 200) alert("Form submitted successfully.");
+	else alert("There was an error with the form.")
+};
